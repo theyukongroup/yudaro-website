f59ee138f8960c8d36c1b7275a672e9fb155fa69
theyukongroup <orders@theyukongroup.com>
2026-09-21 15:17:18 -0500

    Update layout.tsx

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from 'next';
 export const metadata: Metadata = {
-  title: 'Nexavoris Administration',
+  title: 'Yudaro Administration',
   robots: { index: false, follow: false, nocache: true },
   referrer: 'no-referrer',
 };
